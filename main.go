7a6fcba9f92ffdcf8ead695dc96b5e11bdec677c
tomo0611 <tomo0611@hotmail.com>
2024-03-21 15:21:12 +0900

    VersionとRevisionをつけるように

--- main.go
+++ main.go
@@ -1,11 +1,18 @@
 /*
-Copyright © 2024 NAME HERE <EMAIL ADDRESS>
-
+Copyright © 2024 tomo0611 <tomo0611@hotmail.com>
 */
 package main
 
 import "github.com/tomo0611/words-play/cmd"
 
+var (
+	version  = "v0.0.1"
+	revision = "000000"
+)
+
 func main() {
+	cmd.Version = version
+	cmd.Revision = revision
+
 	cmd.Execute()
 }
