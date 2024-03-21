5450bffd17c0b36997b7966a1efd3271f08d8181
tomo0611 <tomo0611@hotmail.com>
2024-03-21 17:01:08 +0900

    versionコマンドを追加

---
+++ cmd/version.go
@@ -0,0 +1,23 @@
+/*
+Copyright © 2024 NAME HERE <EMAIL ADDRESS>
+*/
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
+
+// versionCmd represents the version command
+var versionCmd = &cobra.Command{
+	Use:   "version",
+	Short: "Print version information",
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Printf("Words Play %s (revision %s)\n", Version, Revision)
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(versionCmd)
+}
